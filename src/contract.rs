b29d0c8f3339ff0e3f0de2d9bf85229d0dbcceb4
PoomSC <poom.genius@gmail.com>
2022-02-18 16:06:01 +0700

    add bulk_balance & bulk_token_iinfo query

--- src/contract.rs
+++ src/contract.rs
@@ -1,11 +1,16 @@
+use crate::error::ContractError;
+use crate::msg::{
+    AddrBalance, AddrBalanceResponse, AddrTokenInfoResponse, ContractAddr, ExecuteMsg,
+    InstantiateMsg, QueryMsg,
+};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{
+    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, QueryRequest, Response, StdResult, Uint128,
+    WasmQuery,
+};
 use cw2::set_contract_version;
-
-use crate::error::ContractError;
-use crate::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{State, STATE};
+use cw20::{BalanceResponse, Cw20QueryMsg, TokenInfoResponse};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:multicall-cw20";
@@ -16,132 +21,78 @@ pub fn instantiate(
     deps: DepsMut,
     _env: Env,
     info: MessageInfo,
-    msg: InstantiateMsg,
+    _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    let state = State {
-        count: msg.count,
-        owner: info.sender.clone(),
-    };
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    STATE.save(deps.storage, &state)?;
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
-        .add_attribute("owner", info.sender)
-        .add_attribute("count", msg.count.to_string()))
+        .add_attribute("owner", info.sender))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    deps: DepsMut,
+    _deps: DepsMut,
     _env: Env,
-    info: MessageInfo,
+    _info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    match msg {
-        ExecuteMsg::Increment {} => try_increment(deps),
-        ExecuteMsg::Reset { count } => try_reset(deps, info, count),
-    }
-}
-
-pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        state.count += 1;
-        Ok(state)
-    })?;
-
-    Ok(Response::new().add_attribute("method", "try_increment"))
-}
-pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        if info.sender != state.owner {
-            return Err(ContractError::Unauthorized {});
-        }
-        state.count = count;
-        Ok(state)
-    })?;
-    Ok(Response::new().add_attribute("method", "reset"))
+    match msg {}
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
-    }
-}
-
-fn query_count(deps: Deps) -> StdResult<CountResponse> {
-    let state = STATE.load(deps.storage)?;
-    Ok(CountResponse { count: state.count })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
-
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
-
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
-    }
-
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
+        QueryMsg::BulkBalance { contracts } => to_binary(&query_bulk_balance(deps, contracts)?),
+        QueryMsg::BulkTokenInfo { contracts } => {
+            to_binary(&query_bulk_token_info(deps, contracts)?)
         }
-
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
-
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
+        _ => to_binary(&pass_query()?),
     }
+}
+
+pub fn pass_query() -> StdResult<Response> {
+    Ok(Response::default())
+}
+
+pub fn query_bulk_balance(
+    deps: Deps,
+    contracts: Vec<AddrBalance>,
+) -> StdResult<Vec<AddrBalanceResponse>> {
+    let mut res: Vec<AddrBalanceResponse> = vec![];
+    for contract in contracts.iter() {
+        let response: BalanceResponse =
+            deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
+                contract_addr: contract.contract_addr.to_string(),
+                msg: to_binary(&Cw20QueryMsg::Balance {
+                    address: contract.address.to_string(),
+                })?,
+            }))?;
+
+        res.push(AddrBalanceResponse {
+            contract_addr: contract.contract_addr.to_string(),
+            address: contract.address.to_string(),
+            response,
+        })
+    }
+    Ok(res)
+}
+
+pub fn query_bulk_token_info(
+    deps: Deps,
+    contracts: Vec<ContractAddr>,
+) -> StdResult<Vec<AddrTokenInfoResponse>> {
+    let mut res: Vec<AddrTokenInfoResponse> = vec![];
+    for contract in contracts.iter() {
+        let token_info_res: TokenInfoResponse =
+            deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
+                contract_addr: contract.contract_addr.to_string(),
+                msg: to_binary(&Cw20QueryMsg::TokenInfo {})?,
+            }))?;
+        res.push(AddrTokenInfoResponse {
+            contract_addr: contract.contract_addr.to_string(),
+            response: token_info_res,
+        })
+    }
+    Ok(res)
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,27 +1,142 @@
+use cosmwasm_std::{Addr, Binary, Uint128};
+use cw0::Expiration;
+use cw20::{AllowanceInfo, BalanceResponse, TokenInfoResponse, AllowanceResponse, MinterResponse, MarketingInfoResponse, DownloadLogoResponse, AllAllowancesResponse, AllAccountsResponse};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg {
-    pub count: i32,
+pub struct ContractAddr {
+    pub contract_addr: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+pub struct AddrBalance {
+    pub contract_addr: String,
+    pub address: String,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AddrAllowance {
+    pub contract_addr: String,
+    pub owner: String,
+    pub spender: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AddrAllAllowance {
+    pub contract_addr: String,
+    pub owner: String,
+    pub start_after: Option<String>,
+    pub limit: Option<u32>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AddrAllAccounts {
+    pub contract_addr: String,
+    pub start_after: Option<String>,
+    pub limit: Option<u32>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    /// Returns the current balance of the given address, 0 if unset.
+    /// Return type: BalanceResponse.
+    BulkBalance { contracts: Vec<AddrBalance> },
+      /// Returns metadata on the contract - name, decimals, supply, etc.
+    /// Return type: TokenInfoResponse.
+    BulkTokenInfo { contracts: Vec<ContractAddr> },
+    /// Only with "allowance" extension.
+    /// Returns how much spender can use from owner account, 0 if unset.
+    /// Return type: AllowanceResponse.
+    BulkAllowance { contracts: Vec<AddrAllowance> },
+    /// Only with "mintable" extension.
+    /// Returns who can mint and the hard cap on maximum tokens after minting.
+    /// Return type: MinterResponse.
+    BulkMinter { contracts: Vec<ContractAddr> },
+    /// Only with "marketing" extension
+    /// Returns more metadata on the contract to display in the client:
+    /// - description, logo, project url, etc.
+    /// Return type: MarketingInfoResponse.
+    BulkMarketingInfo { contracts: Vec<ContractAddr> },
+    /// Only with "marketing" extension
+    /// Downloads the embedded logo data (if stored on chain). Errors if no logo data stored for
+    /// this contract.
+    /// Return type: DownloadLogoResponse.
+    BulkDownloadLogo { contracts: Vec<ContractAddr> },
+    /// Only with "enumerable" extension (and "allowances")
+    /// Returns all allowances this owner has approved. Supports pagination.
+    /// Return type: AllAllowancesResponse.
+    BulkAllAllowances { contracts: Vec<AddrAllAllowance> },
+    /// Only with "enumerable" extension
+    /// Returns all accounts that have balances. Supports pagination.
+    /// Return type: AllAccountsResponse.
+    BulkAllAccounts { contracts: Vec<AddrAllAccounts> },
 }
 
-// We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct MigrateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AddrBalanceResponse {
+    pub contract_addr: String,
+    pub address: String,
+    pub response: BalanceResponse,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AddrTokenInfoResponse {
+    pub contract_addr: String,
+    pub response: TokenInfoResponse
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
+pub struct AddrAllowanceResponse {
+    pub contract_addr: String,
+    pub response: AllowanceResponse
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AddrMinterResponse {
+    pub contract_addr: String,
+    pub response: MinterResponse
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
+pub struct AddrMarketingInfoResponse {
+    pub contract_addr: String,
+    pub response: MarketingInfoResponse
+}
+
+/// When we download an embedded logo, we get this response type.
+/// We expect a SPA to be able to accept this info and display it.
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AddrDownloadLogoResponse {
+    pub contract_addr: String,
+    pub response: DownloadLogoResponse
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AddrAllowanceInfo {
+    pub contract_addr: String,
+    pub response: AllowanceInfo
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AddrAllAllowancesResponse {
+    pub contract_addr: String,
+    pub response: AllAllowancesResponse
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AddrAllAccountsResponse {
+    pub contract_addr: String,
+    pub response: AllAccountsResponse
 }
